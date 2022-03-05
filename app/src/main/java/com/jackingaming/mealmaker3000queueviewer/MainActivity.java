f8d99de8ec6fc421d3fa8469ed56c21d6380264d
jamesgov <blindmice1o3@gmail.com>
2022-03-04 16:08:40 -0800

    first commit

---
+++ app/src/main/java/com/jackingaming/mealmaker3000queueviewer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jackingaming.mealmaker3000queueviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
